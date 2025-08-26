5b534ebed69eb0881f3306f1d0a72fb79f095857
SilverHawk2323 <106720102+SilverHawk2323@users.noreply.github.com>
2025-08-26 14:29:57 +1000

    added gameplay effect

---
+++ Source/Paravivo/Private/Actor/ParavivoEffectActor.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright Paravivo
+
+
+#include "Actor/ParavivoEffectActor.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+
+
+// Sets default values
+AParavivoEffectActor::AParavivoEffectActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+}
+
+// Called when the game starts or when spawned
+void AParavivoEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AParavivoEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (TargetASC == nullptr) return;
+
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+}
---
+++ Source/Paravivo/Public/Actor/ParavivoEffectActor.h
@@ -0,0 +1,31 @@
+﻿// Copyright Paravivo
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ParavivoEffectActor.generated.h"
+
+class UGameplayEffect;
+
+UCLASS()
+class PARAVIVO_API AParavivoEffectActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AParavivoEffectActor();
+
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+};
