7925dbbf0d211f21669d2c8a6ed74c9c5ace32cf
SilverHawk2323 <106720102+SilverHawk2323@users.noreply.github.com>
2025-08-29 10:21:54 +1000

    Asset tags are printed to the screen when item is picked up

--- Source/Paravivo/Private/AbilitySystem/ParavivoAbilitySystemComponent.cpp
+++ Source/Paravivo/Private/AbilitySystem/ParavivoAbilitySystemComponent.cpp
@@ -13,5 +13,12 @@ void UParavivoAbilitySystemComponent::AbilityActorInfoSet()
 void UParavivoAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
 	const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1,8.f,FColor::Red,"Effect Applied!");
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the Widget Controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 8.f, FColor::Red, Msg);
+	}
 }
